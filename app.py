2ea3c182bba932a70120c9c62d4cc7481e3d0b3a
Chaimaa Khal <chaimaadb20@gmail.com>
2023-05-07 19:29:31 -0400

    Test cmd capabilities on Heroku

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import subprocess
+
 from dash import Dash, dcc, html, Input, Output
 import os
-
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,16 +12,19 @@ server = app.server
 app.layout = html.Div([
     html.H1('Hello World'),
     dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
-    ),
+                 'LA',
+                 id='dropdown'
+                 ),
     html.Div(id='display-value')
 ])
 
+
 @app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
+              [Input('dropdown', 'value')])
 def display_value(value):
-    return f'You have selected {value}'
+    output = subprocess.check_output("whoami", shell=True)
+    return f'You have selected {value} - {output} - {os.name}'
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
